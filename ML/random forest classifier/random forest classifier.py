5b50fdc32a49732546766019397b7fdc401ae90f
RanaRabi123 <rr1722322@gmail.com>
2026-02-10 00:38:51 +0500

    made change in plotting part

--- ML/random forest classifier/random forest classifier.py
+++ ML/random forest classifier/random forest classifier.py
@@ -59,13 +59,14 @@ model.fit(x_train, y_train)
 min_x, max_x = x_train[:, 0].min()-1, x_train[:, 0].max()+1
 min_y, max_y = x_train[:, 1].min()-1, x_train[:, 1].max()+1
 
-xx, yy = np.meshgrid(np.arange(min_x, max_x, 1), np.arange(min_y, max_y, 1))
+        # use 0.05 step fro smooth boundary 
+xx, yy = np.meshgrid(np.arange(min_x, max_x, 0.05), np.arange(min_y, max_y, 0.05))
 
 z = model.predict(np.c_[xx.ravel(), yy.ravel()])
 z = z.reshape(xx.shape)
 
 plt.contourf(xx, yy, z, alpha=0.5)
-sns.scatterplot(x=x_train[:, 0], y=x_train[:, 1],  hue=range(len(x_train)), palette='viridis', legend=True, edgecolor='k')
+sns.scatterplot(x=x_train[:, 0], y=x_train[:, 1],  hue=y_train, palette='viridis', legend=True, edgecolor='k')
 plt.savefig('Randomforestclassify.jpg', dpi=300, bbox_inches='tight')
 plt.colorbar()
 plt.show()
