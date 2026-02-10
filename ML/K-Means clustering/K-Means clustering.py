a4ec2dee77533f2f4f15e4ad937491690e59d81b
RanaRabi123 <rr1722322@gmail.com>
2026-02-11 01:37:56 +0500

    code updation

--- ML/K-Means clustering/K-Means clustering.py
+++ ML/K-Means clustering/K-Means clustering.py
@@ -1,46 +1,55 @@
 from sklearn.cluster import KMeans
 from sklearn import datasets
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-
 iris = datasets.load_iris()
+x = iris.data  
 
-X = iris.data[:, [0, 2]]   # sepal length & petal length
+        #  dimensionality reduction to 2 
+pca = PCA(n_components=2)
+x_pca = pca.fit_transform(x)  
+print(x_pca)
 
-        #sum of square error,   measure distance of each data to it's nearest centroid
-sse = []        # it will store inertia values 
+
+sse = []
 k_range = range(1, 10)
 for k in k_range:
     model = KMeans(n_clusters=k, random_state=42)
-    model.fit(X)            # only on X, as it si unsupervosed algorithm
+    model.fit(x)  
     sse.append(model.inertia_)
 
 plt.plot(k_range, sse, marker='o')
 plt.xlabel("Number of Clusters (k)")
 plt.ylabel("SSE / Inertia")
-plt.title("Elbow Method")
+plt.title("Elbow Method (Iris Dataset)")
 plt.show()
 
 
-            # we will take cluster number from the above elbow metho point, which will be best k 
-kmeans = KMeans(n_clusters=3, random_state=66)
-kmeans.fit(X)
-
-labels = kmeans.labels_                 
-centers = kmeans.cluster_centers_       # coordinates of centroids
 
 
 
+optimal_k = 3           # from elbow method point
+kmeans = KMeans(n_clusters=optimal_k, random_state=42)
+kmeans.fit(x)           # fit on all features
+labels = kmeans.labels_
+centers = kmeans.cluster_centers_
+
+        # reduction , so that shape match to x_pca
+centers_pca = pca.transform(centers)
 
 
-                # plotting
-        # means select only those data points that has labels 0,1,2 . from the column 0 which is sepal_length and 1 for petal_length
-plt.scatter(X[labels == 0, 0], X[labels == 0, 1])
-plt.scatter(X[labels == 1, 0], X[labels == 1, 1])
-plt.scatter(X[labels == 2, 0], X[labels == 2, 1])
 
-plt.scatter(centers[:, 0], centers[:, 1], marker='+', s=200)    # s for centroid size
-plt.xlabel("Sepal Length")
-plt.ylabel("Petal Length")
-plt.title("K-Means Clustering (Iris Dataset)")
+        # plotting
+plt.scatter(x_pca[labels == 0, 0], x_pca[labels == 0, 1], color='r', label=f'Cluster {1}')
+plt.scatter(x_pca[labels == 1, 0], x_pca[labels == 1, 1], color='g', label=f'Cluster {2}')
+plt.scatter(x_pca[labels == 2, 0], x_pca[labels == 2, 1], color='y', label=f'Cluster {3}')
+
+        # Plot centroids, s for centorid size
+plt.scatter(centers_pca[:, 0], centers_pca[:, 1], marker='+', s=200, color='black', label='Centroids')
+
+plt.xlabel("PCA Component 1")
+plt.ylabel("PCA Component 2")
+plt.title("K-Means Clustering with PCA (Iris Dataset)")
+plt.legend()
 plt.show()
